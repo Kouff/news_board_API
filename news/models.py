382be6f33da4cc26c433361e192434db8ae4439b
Kouff <thematuhaimmortal@gmail.com>
2020-07-22 21:29:03 +0300

    add models Post and Comment

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    link = models.URLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    amount_of_upvotes = models.SmallIntegerField()
+    author_name = models.CharField(max_length=50)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=50)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
